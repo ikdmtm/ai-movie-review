d734990c24c12db928bc4e2fb653bebd7d83db72
ikdmtm <ikdmtm01@gmail.com>
2023-03-27 03:10:30 +0900

    ♻️ ハンバーガーメニューの改良

--- ai-movie-review/src/components/Header/Header.jsx
+++ ai-movie-review/src/components/Header/Header.jsx
@@ -1,20 +1,14 @@
 import styles from "src/components/Header/Header.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Header = (props) => {
   return (
     <header className={styles.header}>
-      <p>AI映画レビュー</p>
-      {props.open ? (
-        <Image
-          src="/closeMenu.png"
-          alt="Picture of the author"
-          width={32}
-          height={32}
-          className={styles.button}
-          onClick={props.handleMenu}
-        />
-      ) : (
+      <Link href="/">
+        <p>映画検索</p>
+      </Link>
+      {props.open ? null : (
         <Image
           src="/openMenu.png"
           alt="Picture of the author"
