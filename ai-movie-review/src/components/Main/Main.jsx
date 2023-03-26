ca1dff52a88abba32bf3206ad6ddfb5b068679bd
ikdmtm <ikdmtm01@gmail.com>
2023-03-27 03:18:28 +0900

    ✨ カテゴリーのリストの追加とリンクの作成

--- ai-movie-review/src/components/Main/Main.jsx
+++ ai-movie-review/src/components/Main/Main.jsx
@@ -1,37 +1,63 @@
 import styles from "src/components/Main/Main.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { Header } from "src/components/Header/Header.jsx";
 import { Footer } from "src/components/Footer/Footer.jsx";
 import { NavMenu } from "src/components/NavMenu/NavMenu.jsx";
 import { Items } from "src/components/Items/Items.jsx";
 
+const categories = [
+  { id: 28, name: "アクション" },
+  { id: 12, name: "アドベンチャー" },
+  { id: 16, name: "アニメーション" },
+  { id: 35, name: "コメディ" },
+  { id: 80, name: "クライム" },
+  { id: 99, name: "ドキュメンタリー" },
+  { id: 18, name: "ドラマ" },
+  { id: 10751, name: "ファミリー" },
+  { id: 14, name: "ファンタジー" },
+  { id: 36, name: "歴史" },
+  { id: 27, name: "ホラー" },
+  { id: 10402, name: "音楽" },
+  { id: 9648, name: "ミステリー" },
+  { id: 10749, name: "ロマンス" },
+  { id: 878, name: "サイエンスフィクション" },
+  { id: 10770, name: "テレビ映画" },
+  { id: 53, name: "スリラー" },
+  { id: 10752, name: "戦争" },
+  { id: 37, name: "西洋" },
+];
+
 export const Main = (props) => {
-  const callApi = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await res.json();
-    console.log(users[0].name);
-  };
-  const users = callApi();
-  console.log(users[0]);
+  console.log("Main", props);
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <Header handleMenu={props.handleMenu} open={props.open} />
-      {props.open ? <NavMenu handleMenu={props.handleMenu} /> : null}
+      {props.open ? <NavMenu handleMenu={props.handleMenu} categories={categories} /> : null}
       <div>
         <ul className={styles.pcNavMenu}>
-          <li>SF</li>
-          <li>アクション</li>
-          <li>恋愛</li>
-          <li>コメディ</li>
-          <li>スリラー</li>
-          <li>ホラー</li>
-          <li>アニメ</li>
+          {/* {genres.map((genre) => (
+            <li>{genre.name}</li>
+          ))} */}
+          <Link href={"/popular"}>
+            <li className={router.route == "/popular" ? styles.pageTitle : null}>最近の人気</li>
+          </Link>
+          <Link href={"/"}>
+            <li className={router.route == "/" ? styles.pageTitle : null}>評価が高い映画</li>
+          </Link>
+          <Link href={"/now_playing"}>
+            <li className={router.route == "/now_playing" ? styles.pageTitle : null}>上映中</li>
+          </Link>
+          <Link href={"/upcoming"}>
+            <li className={router.route == "/upcoming" ? styles.pageTitle : null}>近日公開</li>
+          </Link>
         </ul>
       </div>
-      <div className={styles.title}>
-        <p>- movie -</p>
-      </div>
+
       <main className={styles.main}>
-        <Items />
+        <Items moviesData={props.moviesData} />
       </main>
       <Footer />
     </div>
