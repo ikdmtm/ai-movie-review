afca09534e4d48b9a5de08afbf8e68c38b0c3db6
ikdmtm <ikdmtm01@gmail.com>
2023-03-29 03:11:29 +0900

    ♻️ 関数のメモ化

--- ai-movie-review/src/components/Items/Items.jsx
+++ ai-movie-review/src/components/Items/Items.jsx
@@ -1,27 +1,29 @@
 import styles from "src/components/Items/Items.module.css";
 import Image from "next/image";
 import Link from "next/link";
-
-const getCategory = (item, categories) => {
-  const ids = item.genre_ids;
-  let genreName = [];
-  ids.forEach((id) => {
-    categories.forEach((genre) => {
-      if (id == genre.id) {
-        genreName = [...genreName, genre.name];
-      }
-    });
-  });
-  return genreName;
-};
+import { useCallback } from "react";
 
 export const Items = (props) => {
+  console.log("props", props);
+  const getCategory = useCallback((item, categories) => {
+    const ids = item.genre_ids;
+    let genreName = [];
+    ids.forEach((id) => {
+      categories.forEach((genre) => {
+        if (id == genre.id) {
+          genreName = [...genreName, genre.name];
+        }
+      });
+    });
+    return genreName;
+  }, []);
+
   return (
     <div className={styles.items}>
-      {props.moviesData ? (
+      {props.moviesData.length ? (
         props.moviesData.map((item) => {
           return (
-            <div className={styles.item} key={item.src}>
+            <div className={styles.item} key={item.id}>
               <Link href={"/movies/" + `${item.id}`}>
                 <div className={styles.image}>
                   <Image
@@ -35,7 +37,11 @@ export const Items = (props) => {
                   <div className={styles.subDiscription}>
                     <div className={styles.categories}>
                       {getCategory(item, props.categories).map((cate) => {
-                        return <p className={styles.category}>{cate}</p>;
+                        return (
+                          <p className={styles.category} key={item.src}>
+                            {cate}
+                          </p>
+                        );
                       })}
                     </div>
                     <p className={styles.year}>{item.release_date}</p>
