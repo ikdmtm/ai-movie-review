bd11c2987cd5ff9307834e1ce83b6243bca3fa4e
ikdmtm <ikdmtm01@gmail.com>
2023-05-02 18:18:06 +0900

    ♻️ タイトル変更

--- cinema-searcher/src/components/Header/Header.jsx
+++ cinema-searcher/src/components/Header/Header.jsx
@@ -3,27 +3,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const Header = (props) => {
-  console.log("header", props);
+    console.log("header", props);
 
-  return (
-    <header className={styles.header}>
-      <Link href="/">
-        <p>映画検索</p>
-      </Link>
-      <div className="inputForm">
-        <input type="search" onChange={props.handleChange} value={props.text}></input>
-        <button onClick={props.handleClickSearch}>検索</button>
-      </div>
-      {props.open ? null : (
-        <Image
-          src="/openMenu.png"
-          alt="Picture of the author"
-          width={32}
-          height={32}
-          className={styles.button}
-          onClick={props.handleMenu}
-        />
-      )}
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <Link href="/">
+                <p>Cinema Searcher</p>
+            </Link>
+            <div className="inputForm">
+                <input type="search" onChange={props.handleChange} value={props.text}></input>
+                <button onClick={props.handleClickSearch}>検索</button>
+            </div>
+            {props.open ? null : (
+                <Image
+                    src="/openMenu.png"
+                    alt="Picture of the author"
+                    width={32}
+                    height={32}
+                    className={styles.button}
+                    onClick={props.handleMenu}
+                />
+            )}
+        </header>
+    );
 };
